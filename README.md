d0b9db7731f14985fb29ebd5b76694a49e9d5ca8
Martin Cook <33993749+martycook@users.noreply.github.com>
2024-12-24 08:35:38 -0600

    Add project files.

---
+++ README.md
@@ -0,0 +1,24 @@
+## CodeGator.DependencyInjection: 
+---
+
+
+#### What does it do?
+This package contains dependency injection extensions used by other CodeGator solutions.
+
+#### What platform(s) does it support?
+* .NET 9.x or higher
+
+#### How do I install it?
+The binary is hosted on [NuGet](https://www.nuget.org/packages/Codegator.DependencyInjection/). To install the package using the NuGet package manager:
+
+PM> Install-Package CodeGator.DependencyInjection
+
+#### How do I contact you?
+If you've spotted a bug in the code please use the project Issues [HERE](https://github.com/CodeGator/CodeGator.DependencyInjection/issues)
+
+We also have a discussion group [HERE](https://github.com/CodeGator/CodeGator.DependencyInjection/discussions)
+
+##### We also blog about projects like this one on our website, [HERE](http://www.codegator.com)
+---
+#### Disclaimer
+This package and it's contents are experimental in nature. There is no official support. Use at your own risk.
---
+++ src/CodeGator.DependencyInjection/ServiceLocator.cs
@@ -0,0 +1,71 @@
+﻿
+#pragma warning disable IDE0130
+namespace System;
+#pragma warning restore IDE0130
+
+/// <summary>
+/// This class provides global access to a service provider.
+/// </summary>
+public sealed class ServiceLocator : IServiceProvider
+{
+    // *******************************************************************
+    // Fields.
+    // *******************************************************************
+
+    #region Fields
+
+    /// <summary>
+    /// This field contains the inner service provider for this class.
+    /// </summary>
+    internal static IServiceProvider? _innerServiceProvider = null;
+
+    /// <summary>
+    /// This field contains the singleton instance of this class.
+    /// </summary>
+    private static ServiceLocator? _instance = null;
+
+    #endregion
+
+    // *******************************************************************
+    // Constructors.
+    // *******************************************************************
+
+    #region Constructors
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ServiceLocator"/>
+    /// class.
+    /// </summary>
+    private ServiceLocator() { }
+
+    #endregion
+
+    // *******************************************************************
+    // Public methods.
+    // *******************************************************************
+
+    #region Public methods
+
+    /// <summary>
+    /// This method returns the singleton instance of this class.
+    /// </summary>
+    /// <returns>The singleton instance of this class.</returns>
+    public static ServiceLocator Instance()
+    {
+        if (_instance is null)
+        {
+            _instance = new ServiceLocator();
+        }
+        return _instance;
+    }
+
+    // *******************************************************************
+
+    /// <inheritdoc/>
+    public object? GetService(Type serviceType)
+    {
+        return _innerServiceProvider?.GetService(serviceType);
+    }
+
+    #endregion
+}
